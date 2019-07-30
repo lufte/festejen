9571f936083ce977c44182466eb2a33494a0a9b6
lufte <javierayres@gmail.com>
2019-07-29 23:28:56 -0300

    Update bot to use new generator and remove token from code

--- telegrambot.py
+++ telegrambot.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 import pickle
-import re
 import os
 from parser import generator
 from telegram.ext import *
 
 
-word_pattern = re.compile('\w+(?=\W*$)')
-with open(os.path.join(os.path.dirname(__file__),
-                       './index.pickle'), 'rb') as f:
-    index = pickle.load(f)
-
-
 def handler(bot, update):
     try:
-        last_word = word_pattern.search(update.message.text).group(0).lower()
-        message = generator.build(index, start_word=last_word)
-    except (KeyError, TypeError, AttributeError):
-        message = generator.build(index)
+        message = generator.build(index, seed=update.message.text)
     except Exception as e:
         message = str(e)
     bot.sendMessage(update.message.chat_id, text=message)
 
 
 if __name__ == '__main__':
-    updater = Updater('931610532:AAE1yn9-oonUg_E2wZBJRs7JnOXxOp6vvCk')
+    with open(os.path.join(os.path.dirname(__file__),
+                           './index.pickle'), 'rb') as f:
+        index = pickle.load(f)
+    with open(os.path.join(os.path.dirname(__file__),
+                           './telegram-token')) as f:
+        token = f.read().strip()
+
+    updater = Updater(token)
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.all, handler))
+    dp.add_handler(MessageHandler(Filters.text | Filters.command, handler))
     updater.start_polling()
     updater.idle()
