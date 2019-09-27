c0f0d77b6910db47bfc7f5485eda4323e3f1fd5b
lufte <javierayres@gmail.com>
2019-09-26 23:20:51 -0300

    Add license

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,3 +1,8 @@
+# Copyright © 2019 Javier Ayres
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the LICENSE file for more details.
+
 import re
 from datetime import datetime
 from .dbutils import get_connection
